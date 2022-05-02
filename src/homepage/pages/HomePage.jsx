593f51da6bc2a0b6d64d6e99ba9d7d16f1dedb40
itscyborg <ninjahax0@gmail.com>
2022-05-02 18:23:52 +0530

    update our menu-items to nevigate withRouter HOC

---
+++ src/homepage/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Directory } from "../../component/directory/directory";
+
+import "./HomePage.scss";
+
+const HomePage = () => (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  );
+  
+  export default HomePage;
