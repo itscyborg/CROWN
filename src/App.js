f504f068e0cf8b3d5c135f8a35495ed126af0b7e
itscyborg <ninjahax0@gmail.com>
2022-05-03 09:39:59 +0530

    added header and sign-in component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import Header from "./component/header/header";
 
 import HomePage from "./pages/homepage/HomePage";
 import ShopPage from "./pages/shop/shop";
+import SignInAndSignUp from "./pages/sign-in and sign-up/sign-in-and-sign-up";
 import "./App.css";
 
 
 function App() {
   return (
     <div>
+    <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUp} />
+
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in and sign-up/sign-in-and-sign-up.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignIn from "../../component/sign-in/sign-in";
+
+
+import "./sign-in-and-sign-up.scss"
+
+const SignInAndSignUp = ()=> (
+    <div className="sign-in-and-sign-up">
+    <SignIn/>
+    </div>
+)
+export default SignInAndSignUp;
